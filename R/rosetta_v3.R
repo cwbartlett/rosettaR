34829ef86b29b2bedded48c1726a1a28d8cb0eaa
Galdo, Brendan <brendan.m.galdo@gmail.com>
2023-04-17 11:47:25 -0400

    added lavaan backend

--- R/rosetta_v3.R
+++ R/rosetta_v3.R
@@ -10,11 +10,8 @@
 #' @return List of dataframes which contain factor scores.
 #'
 #' @import lavaan
-#' @importFrom sem sem fscores stdCoef
 #' @importFrom DoE.wrapper lhs.design
-#'
 #' @importFrom Matrix nearPD
-#' @importFrom easyr fac2char
 #' @importFrom dplyr bind_rows
 #'
 #'
@@ -43,9 +40,9 @@
 #'  )
 #'
 
-rosetta.v3 <- function(d,
-                       factor_structure,
-                       missing_corr='normal') {
+rosetta <- function(d,
+                    factor_structure,
+                    missing_corr='normal') {
   # Check arguments
   if(!all(unlist(lapply(d, is.data.frame)))) {
     stop("Check the 'd' argument in function rosetta::rosetta(). 'd' needs to be a list of dataframes.")
@@ -153,22 +150,28 @@ rosetta.v3 <- function(d,
       intersect(names(x), y)
     })
 
-    # the constrained sem RAM text
-    lavaan_model_c <- get_lavaan_model_text(constrained_struc, unconstrained_fit)
+    # the constrained model text
+    lavaan_model_constrained <- get_lavaan_model_text(constrained_struc,
+                                                      unconstrained_fit)
 
     # observed pairwise complete covariance matrix
     obs_cov <- get_obs_cov(x)
+    tmp_unlist = unlist(constrained_struc,recursive = TRUE)
+    obs_cov_subset = obs_cov[tmp_unlist,tmp_unlist]
+
 
     ## the overall lavaan fit
-    constrained_fit <- lavaan::cfa(model = lavaan_model_c,
-                                    sample.cov = obs_cov,
-                                    sample.nobs = nrow(data))
+    constrained_fit <- lavaan::cfa(model = lavaan_model_constrained,
+                                   sample.cov = obs_cov,
+                                   sample.nobs = nrow(data))
 
     # model results
-    constrained_factor_scores <- lavaan::lavPredict(constrained_fit, newdata = x)
+    constrained_factor_scores <-
+      lavaan::lavPredict(constrained_fit, newdata = x)
 
-    constrained_factor_cov <- get_fac_cov_estimates_lavaan(constrained_fit,
-                                                           constrained_struc)
+    constrained_factor_cov <-
+      get_fac_cov_estimates_lavaan(constrained_fit,
+                                   constrained_struc)
 
     list(
       constrained_fit = constrained_fit,
@@ -188,95 +191,11 @@ rosetta.v3 <- function(d,
 }
 
 # Returns a character vector of the 'RAM' model for rosetta
-sem_model <- function(factor_structure, sem_object = NULL) {
-  ## check arguments
-  if(length(names(factor_structure)) != length(factor_structure) || any(names(factor_structure) == "")) {
-    stop("Check the 'factor_structure' argument in function rosetta:::sem_model(). 'factor_structure' needs to be a named list.")
-  }
-  ## Character values and inputs for SEM model
-  vars <- stack(factor_structure)[["values"]]
-  factors <- as.character(stack(factor_structure)[["ind"]])
-  unique_factors <- unique(factors)
-  n_factors <- length(factor_structure)
-  n_vars <- length(vars)
-  coefs <- paste("lam_", vars, sep = "")
-  var_params <- paste("e_", vars, sep = "")
-  fac_diag_values <- rep(1, n_factors)
-  if (n_factors > 1) {
-    factor_combs <- combn(unique_factors, 2)
-  } else {
-    factor_combs <- matrix(unique_factors)
-  }
-  n_combs <- ncol(factor_combs)
-  fac_cor_params <- character(n_combs) # Initialize empty vector
-  for (i in 1:n_combs) {
-    fac_cor_params[i] <- paste("f_", paste(factor_combs[,i], collapse = "_"), sep = "")
-  }
-  if(is.null(sem_object)) { # Set to NA for unconstrained model
-    fac_cor_values <- rep(NA, n_combs)
-  } else { # Set to estimated values for constrained model
-    if (n_factors > 1) {
-      fac_cor_values <- fac_cov_estimates(sem_object) # Store dataframe of factor covariance estimates
-      fac_cor_values <- fac_cor_values[match(fac_cor_params, fac_cor_values$covariance), ] # Make sure the covariance values are matched/ordered to the correct covariance
-      fac_cor_values <- na.omit(fac_cor_values) # Remove any factor covariances that are not needed/matched
-      fac_cor_values <- fac_cor_values$estimate
-      fac_cor_params <- rep(NA, n_combs)
-    }
-  }
-
-  ## Create the dataframe
-  ### Columns
-  if (n_factors > 1) {
-    left <- c(factors, vars, unique_factors, factor_combs[1,])
-    right <- c(vars, vars, unique_factors, factor_combs[2,])
-    arrow <- c(rep("->", n_vars), rep("<->", n_vars), rep("<->", n_factors), rep("<->", n_combs))
-    param <- c(coefs, var_params, rep(NA, n_factors), fac_cor_params)
-    value <- c(rep(NA, n_vars), rep(NA, n_vars), fac_diag_values, fac_cor_values)
-  } else {
-    left <- c(factors, vars, unique_factors)
-    right <- c(vars, vars, unique_factors)
-    arrow <- c(rep("->", n_vars), rep("<->", n_vars), rep("<->", n_factors))
-    param <- c(coefs, var_params, rep(NA, n_factors))
-    value <- c(rep(NA, n_vars), rep(NA, n_vars), fac_diag_values)
-  }
-
-  ### Dataframe
-  data <- data.frame(
-    left = left,
-    arrow = arrow,
-    right = right,
-    param = param,
-    value = value,
-    stringsAsFactors = FALSE
-  )
-
-  ### Create Empty character vector that will be filled. This avoids "writing" to disk.
-  if (n_factors > 1) {
-    text <- character(n_vars * 2 + n_factors + n_combs)
-  } else {
-    text <- character(n_vars * 2 + n_factors)
-  }
-  ### Fill in character vector
-  for (i in 1:length(text)) { # Specify regression coefficients
-    text[i] <- paste(
-      paste(as.character(data[i,1:3]), collapse = " "),
-      paste(as.character(data[i,4:5]), collapse = ", "),
-      sep = ", "
-    )
-  }
-
-  # Return SEM model
-  model_text <- paste(text, collapse = "\n")
-  specified_model <- sem::specifyModel(text = model_text, quiet = TRUE)
-  specified_model
-}
-
-# Returns a character vector of the 'RAM' model for rosetta
 get_lavaan_model_text <- function(factor_structure,lavaan_obj=NULL) {
 
   ## check arguments
   if(length(names(factor_structure)) != length(factor_structure) || any(names(factor_structure) == "")) {
-    stop("Check the 'factor_structure' argument in function rosetta:::sem_model(). 'factor_structure' needs to be a named list.")
+    stop("Check the 'factor_structure' argument in function rosetta:::get_lavaan_model_text(). 'factor_structure' needs to be a named list.")
   }
 
   x =  factor_structure
@@ -312,6 +231,7 @@ get_lavaan_model_text <- function(factor_structure,lavaan_obj=NULL) {
 
   return(lavaan_text)
 }
+
 # Returns the observed pairwise complete covariance matrix.
 get_obs_cov <- function(d) {
   d <- d[, colSums(is.na(d)) < nrow(d)] # Remove columns which only contain NA
@@ -319,19 +239,7 @@ get_obs_cov <- function(d) {
   obs_cov
 }
 
-# Returns factor covariance estimates from an sem object.
-fac_cov_estimates <- function(sem) {
-  n_factors <- sem$m - sem$n
-  coef <- easyr::fac2char(sem::stdCoef(sem))
-  n_coef <- nrow(sem::stdCoef(sem))
-  n_combins <- ncol(combn(n_factors,2))
-  factor_coef <- coef[(n_coef-n_combins+1):n_coef, 1:2]
-  colnames(factor_coef) <- c("covariance", "estimate")
-  rownames(factor_coef) <- NULL
-  factor_coef
-}
-
-
+# Extract covariance estimates from lavaan model fit
 get_fac_cov_estimates_lavaan <- function(lavaan_model_obj,factor_structure) {
   pars_est = lavaan_model_obj |> lavaan::parameterestimates()
   factor_names = names(factor_structure)
